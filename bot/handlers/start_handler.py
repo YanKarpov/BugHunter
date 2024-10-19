5024fa7d06289b63590b044440fa056132361fe2
YanKarpov <KarpovYAA23@spb.ithub.ru>
2024-10-19 17:33:13 +0300

    MainMenu

--- bot/handlers/start_handler.py
+++ bot/handlers/start_handler.py
@@ -1,11 +1,29 @@
 from aiogram import types, Router
 from aiogram.filters import Command
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 router = Router()
 
 def register_start_handler(dp: Router):
     @router.message(Command('start'))
     async def start_command(message: types.Message):
-        await message.reply("Я – BugHunter, помощник в сборе багов. Сообщи о проблеме и я передам ее нашим тестировщикам!")
+        welcome_text = (
+            "Я – BugHunter, помощник в сборе багов. Сообщи о проблеме и я передам ее "
+            "нашим тестировщикам!\n"
+            "Чтобы начать, выбери категорию:"
+        )
+        
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [
+                InlineKeyboardButton(text="Не открывается", callback_data="category_not_opening"),
+                InlineKeyboardButton(text="Работает некорректно", callback_data="category_not_working"),
+            ],
+            [
+                InlineKeyboardButton(text="Сломалась верстка", callback_data="category_broken_layout"),
+                InlineKeyboardButton(text="Возможно, уязвимость", callback_data="category_vulnerability"),
+            ],
+        ])
+        
+        await message.reply(welcome_text, reply_markup=keyboard)
     
     dp.include_router(router)
