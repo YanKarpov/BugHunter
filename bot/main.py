1462653c213379d7996377ca8772da5cb368e556
YanKarpov <KarpovYAA23@spb.ithub.ru>
2024-10-19 19:11:51 +0300

    create mapping for category and postgre connections

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,14 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from bot.handlers.start_handler import register_start_handler
-from bot.handlers.report_handler import register_report_handler
+from aiogram.fsm.storage.memory import MemoryStorage
+from bot.handlers.start_handler import router as start_router  
 from config import TELEGRAM_TOKEN
 
 bot = Bot(token=TELEGRAM_TOKEN)
-dp = Dispatcher()
+storage = MemoryStorage()  
+dp = Dispatcher(storage=storage)
 
-register_start_handler(dp)
-register_report_handler(dp)
+dp.include_router(start_router)
 
 async def main():
     await dp.start_polling(bot)
